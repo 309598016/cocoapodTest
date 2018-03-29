599296c350b674f9ec2be4ecc8ff478ce4e52801
weixiaoguang <weixiaoguang123@qq.com>
2018-03-29 14:12:13 +0800

    q

---
+++ weixiaoguangCocoapod_test/Classes/CocoaPodTest.h
@@ -0,0 +1,12 @@
+//
+//  CocoaPodTest.h
+//  FBSnapshotTestCase
+//
+//  Created by 晓桄魏 on 2018/3/29.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CocoaPodTest : NSObject
+
+@end
---
+++ weixiaoguangCocoapod_test/Classes/CocoaPodTest.m
@@ -0,0 +1,12 @@
+//
+//  CocoaPodTest.m
+//  FBSnapshotTestCase
+//
+//  Created by 晓桄魏 on 2018/3/29.
+//
+
+#import "CocoaPodTest.h"
+
+@implementation CocoaPodTest
+
+@end
